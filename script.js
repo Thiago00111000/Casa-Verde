7457e9d50f5443dcaa9ca7bcda8ee91db244af19
Thiago <127748415+Thiago00111000@users.noreply.github.com>
2024-07-22 13:08:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,11 +11,3 @@ for(let i=0; i < botoes.length; i++){
         texto[i].classList.add("ativo");
     }
 }
-
-//function comprar() {
-//const width= 800;
-//const height= 600;
-//const left= (screen.width / 2)- (width / 2);
-//const top= (screen.heigth / 2)- (height / 2);
-//window.open ('cadastro.html' , 'pagamento' , 'width=${width},height=${height}, top=${top}, left=${left}');
-//}
